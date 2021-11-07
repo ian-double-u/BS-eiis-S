12278583c3441a531ccd299a3710ca3ad6ef3991
Ian Wehba <56119986+ian-double-u@users.noreply.github.com>
2021-11-07 06:28:13 -0500

    Update urlParser.py

--- urlParser/urlParser.py
+++ urlParser/urlParser.py
@@ -21,49 +21,144 @@ def urlParser(url,json=False):
                      "parameters": {},
                      "fragment": ""}
     
-    # -- port --
-    if re.search(":\d{2,5}", url): 
-        parsedUrl["port"] = strRegexMatch(":\d{2,5}",url)[1:] 
-    
-    # -- query --
-    if "?" in url: 
-        
-        # -- fragment --
-        if "#" in url: 
-            parsedUrl["query"] = strRegexMatch("\?.*?(?=#)",url)[1:] 
-        else: 
-            parsedUrl["query"] = strRegexMatch("(\?.*)",url)[1:]   
+    if "://" in url:
+        if re.search(":\d{2,5}", url): 
+            if "?" in url:
+                if "#" in url:
+                    # --- protocol, port, query, fragment --- 
+                    parsedUrl["protocol"] = strRegexMatch(".+?(?=:\/\/)",url)
+                    parsedUrl["port"] = strRegexMatch(":\d{2,5}",url)[1:]
+                    parsedUrl["FQDN"] = strRegexMatch(":\/\/.+?(?=:)",url)[3:]
+                    parsedUrl["path"] = strRegexMatch("\d\/.*\?",url)[1:-1]
+                    parsedUrl["query"] = strRegexMatch("\?.*?(?=#)",url)[1:]
+                    parsedUrl["fragment"] = strRegexMatch("#.*",url)[1:]
+                
+                else:
+                    # --- protocol, port, query --- 
+                    parsedUrl["protocol"] = strRegexMatch(".+?(?=:\/\/)",url)
+                    parsedUrl["port"] = strRegexMatch(":\d{2,5}",url)[1:]
+                    parsedUrl["FQDN"] = strRegexMatch(":\/\/.+?(?=:)",url)[3:]
+                    parsedUrl["path"] = strRegexMatch("\d\/.*\?",url)[1:-1]
+                    parsedUrl["query"] = strRegexMatch("(\?.*)",url)[1:]
+                   
+            else:
+                if "#" in url:
+                    # --- protocol, port, fragment ---- 
+                    parsedUrl["protocol"] = strRegexMatch(".+?(?=:\/\/)",url) 
+                    parsedUrl["port"] = strRegexMatch(":\d{2,5}",url)[1:]
+                    parsedUrl["FQDN"] = strRegexMatch(":\/\/.+?(?=:)",url)[3:] 
+                    parsedUrl["path"] = strRegexMatch("\d\/.*#",url)[1:-1] 
+                    parsedUrl["fragment"] = strRegexMatch("#.*",url)[1:]
+                    
+                    
+                else:
+                    # --- protocol, port --- 
+                    parsedUrl["protocol"] = strRegexMatch(".+?(?=:\/\/)",url) 
+                    parsedUrl["port"] = strRegexMatch(":\d{2,5}",url)[1:] 
+                    parsedUrl["FQDN"] = strRegexMatch(":\/\/.+?(?=:)",url)[3:] 
+                    parsedUrl["path"] = strRegexMatch("\d\/.*$",url)[1:] 
+                    
+        else:
+            if "?" in url:
+                if "#" in url:
+                    # --- protocol, query, fragment --- 
+                    parsedUrl["protocol"] = strRegexMatch(".+?(?=:\/\/)",url) 
+                    parsedUrl["FQDN"] = strRegexMatch(":\/\/.+?(?=((\/|$)|(\?|#)))",url)[3:] 
+                    parsedUrl["path"] = strRegexMatch("[a-z]\/.*\?",url)[1:-1] 
+                    parsedUrl["query"] = strRegexMatch("\?.*?(?=#)",url)[1:] 
+                    parsedUrl["fragment"] = strRegexMatch("#.*",url)[1:] 
+                    
+                    
+                else:
+                    # --- protocol, query --- 
+                    parsedUrl["protocol"] = strRegexMatch(".+?(?=:\/\/)",url) 
+                    parsedUrl["FQDN"] = strRegexMatch(":\/\/.+?(?=((\/|$)|(\?|#)))",url)[3:] 
+                    parsedUrl["path"] = strRegexMatch("[a-z]\/.*\?",url)[1:-1] 
+                    parsedUrl["query"] = strRegexMatch("\?.*",url)[1:] 
+                           
+            else:
+                if "#" in url:
+                    # --- protocol, fragment --- 
+                    parsedUrl["protocol"] = strRegexMatch(".+?(?=:\/\/)",url) 
+                    parsedUrl["FQDN"] = strRegexMatch(":\/\/.+?(?=((\/|$)|(\?|#)))",url)[3:] 
+                    parsedUrl["path"] = strRegexMatch("[a-z]\/.*#",url)[1:-1] 
+                    parsedUrl["fragment"] = strRegexMatch("#.*",url)[1:] 
+                    
+                    
+                else:
+                    # --- protocol --- 
+                    parsedUrl["protocol"] = strRegexMatch(".+?(?=:\/\/)",url) 
+                    parsedUrl["FQDN"] = strRegexMatch(":\/\/.+?(?=((\/|$)|(\?|#)))",url)[3:] 
+                    parsedUrl["path"] = strRegexMatch("[a-z]\/.*$",url)[1:] 
             
-        # --- parameters ---
+    else:
+        if re.search(":\d{2,5}", url): 
+            if "?" in url:
+                if "#" in url:
+                    # --- port, query, fragment --- 
+                    parsedUrl["port"] = strRegexMatch(":\d{2,5}",url)[1:]
+                    parsedUrl["FQDN"] = strRegexMatch(".+?(?=:)",url)
+                    parsedUrl["path"] = strRegexMatch("\d\/.*\?",url)[1:-1] 
+                    parsedUrl["query"] = strRegexMatch("\?.*?(?=#)",url)[1:] 
+                    parsedUrl["fragment"] = strRegexMatch("#.*",url)[1:] 
+                    
+                else:
+                    # --- port, query --- 
+                    parsedUrl["port"] = strRegexMatch(":\d{2,5}",url)[1:]
+                    parsedUrl["FQDN"] = strRegexMatch(".+?(?=:)",url)
+                    parsedUrl["path"] = strRegexMatch("\d\/.*\?",url)[1:-1] 
+                    parsedUrl["query"] = strRegexMatch("(\?.*)",url)[1:] 
+                        
+            else:
+                if "#" in url:
+                    # --- port, fragement --- 
+                    parsedUrl["port"] = strRegexMatch(":\d{2,5}",url)[1:]
+                    parsedUrl["FQDN"] = strRegexMatch(".+?(?=:)",url) 
+                    parsedUrl["path"] = strRegexMatch("\d\/.*#",url)[1:-1] 
+                    parsedUrl["fragment"] = strRegexMatch("#.*",url)[1:] 
+                    
+                else:
+                    # --- port --- 
+                    parsedUrl["port"] = strRegexMatch(":\d{2,5}",url)[1:]
+                    parsedUrl["FQDN"] = strRegexMatch(".+?(?=:)",url) 
+                    parsedUrl["path"] = strRegexMatch("\d\/.*$",url)[1:] 
+                     
+        else:
+            if "?" in url:
+                if "#" in url:
+                    # --- query, fragment --- 
+                    parsedUrl["FQDN"] = strRegexMatch(".+?(?=\?)",url) 
+                    parsedUrl["path"] = strRegexMatch("[a-z]\/.*\?",url)[1:-1] 
+                    parsedUrl["query"] = strRegexMatch("\?.*?(?=#)",url)[1:] 
+                    parsedUrl["fragment"] = strRegexMatch("#.*",url)[1:] 
+                    
+                else:
+                    # --- query --- 
+                    parsedUrl["FQDN"] = strRegexMatch(".+?(?=\?)",url) 
+                    parsedUrl["path"] = strRegexMatch("[a-z]\/.*\?",url)[1:-1] 
+                    parsedUrl["query"] = strRegexMatch("\?.*",url)[1:] 
+                           
+            else:
+                if "#" in url:
+                    # --- fragement ---
+                    parsedUrl["FQDN"] = strRegexMatch(".+?(?=(\/|#))",url) 
+                    parsedUrl["path"] = strRegexMatch("[a-z]\/.*#",url)[1:-1] 
+                    parsedUrl["fragment"] = strRegexMatch("#.*",url)[1:] 
+                    
+                else:
+                    # --- ---
+                    parsedUrl["FQDN"] = strRegexMatch(".+?(?=\/)",url) 
+                    parsedUrl["path"] = strRegexMatch("[a-z]\/.*$",url)[1:] 
+                    
+    # --- parameters ---
+    if parsedUrl["query"]:
         parameter_split = parsedUrl["query"].split("&")
         parameter_pairs = [(strRegexMatch(".+?(?=\=)",i),strRegexMatch("\=(.*)",i)[1:]) for i in parameter_split]
         parsedUrl["parameters"] = {key:val for (key,val) in parameter_pairs}
-    
-    # -- fragment --
-    if "#" in url: 
-        parsedUrl["fragment"] = strRegexMatch("#.*",url)[1:] 
         
-    # -- protocol --
-    if re.search(".+?(?=:\/\/)", url): 
-        parsedUrl["protocol"] = strRegexMatch(".+?(?=:\/\/)",url) 
-        
-    # --- domains ---
-    if re.search(".+?(?=:\/\/)", url):
-        if re.search(":\d{2,5}", url):
-            parsedUrl["FQDN"] = strRegexMatch(":\/\/.+?(?=:)",url)[3:]
-        else:
-            if "?" in url:
-                parsedUrl["FQDN"] = strRegexMatch(":\/\/.+?(?=\?)",url)[3:]
-            else:
-                parsedUrl["FQDN"] = strRegexMatch(":\/\/.+?(?=(\/|$))",url)[3:]
-    else:
-        if "?" in url:
-            parsedUrl["FQDN"] = strRegexMatch(".+?(?=\?)",url)
-        else:
-            if "#" in url:
-                parsedUrl["FQDN"] = strRegexMatch(".+?(?=#)",url)
-            else:
-                parsedUrl["FQDN"] = url
+    # --- path correction ---
+    if parsedUrl["path"] == "":
+        parsedUrl["path"] = "/"
         
     # --- (subdomain(s)), domain, (SLD(s)), TLD ---
     domain_split = parsedUrl["FQDN"].split(".")
@@ -81,47 +176,13 @@ def urlParser(url,json=False):
     "SLD": "",
     "TLD": "",
     "subdomin": "",
-    """
+    """    
     
-    # --- path ---
-    if re.search(":\d{2,5}", url):
-        if "?" in url:
-            parsedUrl["path"] = strRegexMatch("\d\/.*\?",url)[1:-1]
-        else:
-            if "#" in url:
-                parsedUrl["path"] = strRegexMatch("\d\/.*#",url)[1:]
-            else:
-                parsedUrl["path"] = strRegexMatch("\d\/.*$",url)[1:]
-    else:
-        if "?" in url:
-            parsedUrl["path"] = strRegexMatch("[a-z]\/.*\?",url)[1:-1]
-        else:
-            if "#" in url:
-                parsedUrl["path"] = strRegexMatch("[a-z]\/.*#",url)[1:]
-            else:
-                if re.search("[a-z]\/.*$", url):
-                    parsedUrl["path"] = parsedUrl["path"] = strRegexMatch("[a-z]\/.*$",url)[1:]
-    
-    # -- return --
+    # --- return ---
     if json:
         return parsedUrl
     else:
         print(f"url: {url}\n")
         for key in parsedUrl:
             if parsedUrl[key]:
-                print(f"{key}: {parsedUrl[key]}")
-                
-test_list = [
-     "https://video.google.co.uk:443/videoplay?docid=-7246927612831078230&hl=en#00h02m30s",
-     "https://video.google.co.uk",
-     "https://video.youtube.co.uk/videoplay?docid=-7246927612831078230&hl=en#00h02m30s",
-     "https://video.facebook.co.uk/videoplay?docid=-7246927612831078230&hl=en",
-     "https://video.snapchat.co.uk/videoplay#00h02m30s",
-     "https://video.twitter.co.uk/videoplay",
-     "https://video.twitter.co.uk/videoplay/v",
-     "https://bhs.beaverton.k12.or.us/",
-]
-
-for u in test_list:
-    print("\n")
-    urlParser(u)
+                print(f"{key}: {parsedUrl[key]}")   
